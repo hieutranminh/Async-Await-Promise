4e51c4a7b146b185b4d19967f659496e905d7262
hieutranminh <32066608+hieutranminh@users.noreply.github.com>
2019-04-12 09:40:17 +0700

    Update Async-Await-Promise.js

--- Async-Await-Promise.js
+++ Async-Await-Promise.js
@@ -63,3 +63,26 @@ async  function  getAPI () {
   console.timeEnd('start');
 }
 getAPI();
+
+// ScrollX & animate jquery----------------------------
+var animateFrame = false
+$(document).on('touchstart', '.home__category', function () {
+  animateFrame = true
+})
+$(document).on('touchmove', '.home__category', function () {
+  animateFrame = false
+})
+$(document).on('click touchend', '.home__category span', function (e) {
+  if (e.type === 'click') animateFrame = true
+  if (animateFrame) {
+    $(this).addClass('active')
+    var scroller = $('.home__category')
+    $('.home__category span').not($(this)).removeClass('active')
+    var scrollTo = $(this).offset().left - scroller.width() / 2 + $(this).width() / 2 + scroller.scrollLeft()
+    scroller.stop().animate({'scrollLeft': scrollTo}, 300)
+    scroller.css('overflow-x', 'hidden')
+    setTimeout(function () {
+      scroller.css('overflow-x', 'auto')
+    }, 100)
+  }
+})
