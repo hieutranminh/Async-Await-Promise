908f67297afd6c3f65fbf919ab90230e7d5d6176
hieutranminh <32066608+hieutranminh@users.noreply.github.com>
2019-04-12 17:18:22 +0700

    Update VeeValidate.js

--- VeeValidate.js
+++ VeeValidate.js
@@ -1,20 +1,21 @@
+// VEE VALIDATE WRITE IN HTML
 <div class="form-group">
-                  <label for="">Tên nhân viên</label>
-                  <input type="text"
-                         v-model="user.ten"
-                         v-validate="{
-                            required: true,
-                            min: 5 ,
-                            regex: /^[a-zA-Z0-9\.\@\_]*$/
-                         }"
-                         name="ten"
-                         class="form-control"
-                         :class="{ 'is-invalid': submitted && errors.has('ten') }">
-                  <p v-for="error in errors.items" v-bind:key="`${error.field} - ${error.rule}`">
-                    <span v-if="error.field ==='ten'">
-                      <template v-if="error.rule === 'required'">Ten 0 duoc de trong</template>
-                      <template v-if="error.rule === 'min'">Ban phai nhap tren 5 ki tu</template>
-                      <template v-if="error.rule === 'regex'">Khong duoc co khoang trang</template>
-                    </span>
-                  </p>
-                </div>
+  <label for="">Tên nhân viên</label>
+  <input type="text"
+         v-model="user.ten"
+         v-validate="{
+            required: true,
+            min: 5 ,
+            regex: /^[a-zA-Z0-9\.\@\_]*$/
+         }"
+         name="ten"
+         class="form-control"
+         :class="{ 'is-invalid': submitted && errors.has('ten') }">
+  <p v-for="error in errors.items" v-bind:key="`${error.field} - ${error.rule}`">
+    <span v-if="error.field ==='ten'">
+      <template v-if="error.rule === 'required'">Ten 0 duoc de trong</template>
+      <template v-if="error.rule === 'min'">Ban phai nhap tren 5 ki tu</template>
+      <template v-if="error.rule === 'regex'">Khong duoc co khoang trang</template>
+    </span>
+  </p>
+</div>
