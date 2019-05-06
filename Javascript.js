1ed44b98f9b08514f942eb577f68dd25fffb811f
hieutranminh <32066608+hieutranminh@users.noreply.github.com>
2019-05-06 08:38:01 +0700

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -15,116 +15,119 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("resize", centerBox)
 })
 
+$(document).ready(function () {
 
-<!-- BOOKING -->
-<section id="booking" class="booking">
-    <div class="booking_container">
-        <div class="booking_content">
-            <p>来場予定日・来場人数把握のため、事前予約をお願いしております。<br>
-                お手数おかけしますが下記フォームよりご予約のほど宜しくお願い申し上げます。</p>
-                <a href="#" class="btn_book">
-                    <img src="./assets/images/icon_pen.png" alt="iconpen">
-                    <span>予約はこちら</span>
-                </a>
-        </div>
-    </div>
-</section>
-<footer id="footer_01" class="footer_01">
-    <div class="footer_01_container">
-        <div class="footer_01_content">
-            <h4>ACCESS</h4>
-            <p class="title">アクセス</p>
-            <div class="box">
-                <div class="map"></div>
-                <div class="text">
-                    <div class="box_text">
-                        <p>住所 ：</p>
-                        <span>東京都渋谷区東3-13-11 A-PLACE恵比寿東 9F <br>
-                        JR恵比寿駅西口より徒歩6分 <br>
-                        東京メトロ日比谷線恵比寿駅5番出口より徒歩5分</span>
-                    </div>
-                    <div class="box_text">
-                        <p>TEL ：</p>
-                        <span>03-6821-9393</span>
-                    </div>
-                    <div class="box_text">
-                        <p>FAX ：</p>
-                        <span>03-6821-9395</span>
-                    </div>
-                    <div class="box_text">
-                        <p>URL ：</p>
-                        <span>http://www.gumi.co.jp/</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
-
-
-
-// BOOKING
-.booking {
-  background-color: #e0e0e0;
-  &_container {
-    max-width: 1000px;
-    margin: auto;
-    text-align: center;
-    @include flex;
-    justify-content: center;
-    align-items: center;
+  // set variable
+  window.$btn = {
+    btnOpenMenu: $('.nav_button'),
+    btnOpenMenuMobile: $('.nav_button_mobile'),
+    btnCloseMenu: $('.nav_button_close'),
+    popupMenu: $('.navbar_popup'),
   }
-  &_content {
-    padding: 102px 142px;
-    p {
-      font-size: 19px;
-      font-weight: bold;
-      line-height: 40px;
-      color: #242424;
+
+  function init () {
+    /* Create event click for... */
+    $btn.btnOpenMenu.on('click', function () {
+      $btn.popupMenu.addClass('openMenu')
+    })
+
+    $btn.btnOpenMenuMobile.on('click', function () {
+      $btn.popupMenu.addClass('openMenu')
+    })
+
+    $btn.btnCloseMenu.on('click', function () {
+      $btn.popupMenu.removeClass('openMenu')
+    })
+
+    /* */
+  }
+
+  function stuffleText (nameStuffle) {
+    var stuffleText = $(nameStuffle)
+    for (var i = 0; i < stuffleText.length; i++) {
+      stuffleText.shuffleLetters()
     }
-    a {
-      @include inline-flex;
-      background-color: #9f080d;
-      color: #ffffff;
-      padding: 22px 210px;
-      margin-top: 37px;
-      text-decoration: none;
-      span {
-        font-size: 24px;
-        font-weight: bold;
-        padding-left: 10px;
+  }
+
+  function bindEvent () {
+    window.count = 0
+    window.btnNavigation = {
+      btnMessage: $('.btn-message'),
+      btnManikin: $('.btn-manikin'),
+      btnOnTheTrack: $('.btn-onthetrack'),
+      btnSakana: $('.btn-sakana'),
+      btnBodAi: $('.btn-bodai'),
+      btnAnomado: $('.btn-anomado'),
+    }
+    window.divOffset = {
+      sectionManikin: $('.section-manikin').offset().top,
+      sectionOnthetrack: $('.section-onthetrack').offset().top,
+      sectionSakana: $('.section-sakana').offset().top,
+      sectionBodai: $('.section-bodai').offset().top,
+      sectionAnomado: $('.section-anomado').offset().top,
+    }
+    init()
+
+    $(window).scroll(function () {
+
+      this.currentOffset = $('html,body').scrollTop() + 1;
+
+      if (currentOffset > divOffset.sectionManikin && count < 1) {
+        stuffleText('.stuffle-text-01')
+        self.count++
+      } else if (currentOffset > divOffset.sectionOnthetrack && count < 2) {
+        stuffleText('.stuffle-text-02')
+        self.count++
+      } else if (currentOffset > divOffset.sectionSakana && count < 3) {
+        stuffleText('.stuffle-text-03')
+        self.count++
+      } else if (currentOffset > divOffset.sectionBodai && count < 4) {
+        stuffleText('.stuffle-text-04')
+        self.count++
+      } else if (currentOffset > divOffset.sectionAnomado && count < 5) {
+        stuffleText('.stuffle-text-05')
+        self.count++
       }
-    }
+    })
+
+    // Smooth scroll jquery
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
   }
-}
-// FOOTER 01
-.footer_01 {
-  background-color: #222222;
-  &_container {
-    max-width: 1000px;
-    margin: auto;
-    @include flex;
-    justify-content: center;
-    align-items: center;
-  }
-  &_content {
-    color: #ffffff;
-    h4 {
-      text-align: center;
-    }
-    .title {
-      text-align: center;
-    }
-  }
-}
 
+  bindEvent()
 
-
-
-@mixin inline-flex() {
-  //display: -webkit-box;
-  display: -moz-inline-box;
-  display: -ms-inline-flexbox;
-  display: -webkit-inline-flex;
-  display: inline-flex;
-}
+})
