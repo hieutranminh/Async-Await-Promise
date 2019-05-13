d9d27330b97e986dcf8ca80b21070a629307ca3f
hieutranminh <32066608+hieutranminh@users.noreply.github.com>
2019-05-13 11:24:45 +0700

    Update AnimeJS.js

--- AnimeJS.js
+++ AnimeJS.js
@@ -1,1 +1,34 @@
 
+$(document).ready(function () {
+    anime({
+        // targets element need make animation
+        targets: '.battery-log',
+        // targets more element with array
+        targets: [el, '.ele-01', '.ele-02'],
+        // set value property animation
+        translateX: 250,
+        // set value DETAILS property animation
+        rotate: {
+            // use unit DEG or TURN - example : 360deg || 1turn
+            value: '360deg',
+            // duration animation of element
+            duration: 1800,
+            // easing jquery - example : easeInOutExpo,...
+            easing: 'linear'
+        },
+        // round up the value to decimal
+        round: 1,
+        // easing jquery - example : easeInOutExpo,...
+        easing: 'easeInOutQuad',
+        // time delay before run animation - can write function (el, i, l)
+        delay: 250,
+        // done animation 01 and waiting 1ms - can write function (el, i, l)
+        endDelay: 1000,
+        // loop animation , the same for loop or infinity animation
+        loop: true,
+        // 3 direction : normal , reverse , alternate
+        direction: 'normal',
+        // auto starts the animation true or false
+        autoplay: true,
+    });
+})
