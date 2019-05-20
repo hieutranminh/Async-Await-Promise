74fe811b194c9bbe1acfbc56c8fcef1ceb8b6124
hieutranminh <32066608+hieutranminh@users.noreply.github.com>
2019-05-20 14:20:16 +0700

    Update method-wordpress.php

--- method-wordpress.php
+++ method-wordpress.php
@@ -3,3 +3,15 @@
 
 // gọi đường dẫn đến theme
 <?php echo get_template_directory_uri() ?>
+
+// ngôn ngữ trên thẻ <html>
+<?php language_attributes() ?>
+  
+// tiêu đề title trong head
+<?php bloginfo('name') ?>
+  
+// chứa các hàm , style , js ,... của Wordpress
+<?php wp_head() ?>
+  
+// chứa các hàm , style , js ,... của Wordpress
+<?php wp_footer() ?>
