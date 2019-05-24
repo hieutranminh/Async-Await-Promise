877c69b3aa68bcfedea9de81d01dc0f06ba77b9f
hieutranminh <32066608+hieutranminh@users.noreply.github.com>
2019-05-24 14:30:35 +0700

    Update method-wordpress.php

--- method-wordpress.php
+++ method-wordpress.php
@@ -24,6 +24,34 @@
 // Ảnh đại diện - option gồm (full, large , medium , thumbnail)
 <?php the_post_thumbnail('option') ?>
   
+// Kiếm tra xem bài post hiện tại đã có ảnh hay chưa
+<?php has_post_thumbnail() ?>
+  
+// check xem có bài post không
+<?php have_posts(); ?>
+
+// check xem phải trang POST ko
+<?php is_home(); ?>
+  
+// get các bài post ra
+<?php the_post(); ?>
+  
+// lấy ra tiêu đề page hiện tại 
+<?php echo get_post(get_option("page_for_posts")) -> post_title; ?>
+  
+// get các danh mục (category được tạo ra từ CPT UI) của taxanomy
+<?php get_the_terms( $post -> ID , '$taxanomy') ?>
+  
+// Đếm số lượng của 1 loại bài post (có thể -> publish để lấy ra những bài publish)  
+<?php wp_count_posts('post');?>
+  
+// Phân trang pagination 
+<?php the_posts_pagination( array(
+  'mid_size' => 2,
+  'prev_text' => __( '&laquo;', 'textdomain' ),
+  'next_text' => __( '&raquo;', 'textdomain' ),
+)); ?>
+  
 // Lấy URL của hình ảnh (truyền vào ID của hình ảnh & option : (full, large , medium , thumbnail)) & return mảng (array)
 <?php wp_get_attachment_image_src(get_post_thumbnail_id($post -> ID), 'option') ?>
   
@@ -48,28 +76,10 @@
 // get một khối giao diện ( phải đặt tên file content-welcome )
 <?php get_template_part('template-parts/content','welcome') ?>
   
-// check xem có bài post không
-<?php have_posts(); ?>
-  
-// get các bài post ra
-<?php the_post(); ?>
-  
 // reset vong lap
 <?php wp_reset_query(); ?>
 <?php wp_reset_postdata(); ?>
   
-// get các danh mục (category được tạo ra từ CPT UI) của taxanomy
-<?php get_the_terms( $post -> ID , '$taxanomy') ?>
-  
-// Đếm số lượng của 1 loại bài post (có thể -> publish để lấy ra những bài publish)  
-<?php wp_count_posts('post');?>
-  
-// Phân trang pagination 
-<?php the_posts_pagination( array(
-  'mid_size' => 2,
-  'prev_text' => __( '&laquo;', 'textdomain' ),
-  'next_text' => __( '&raquo;', 'textdomain' ),
-)); ?>
   
   
 -----------------------------FUNCTION.php------------------------
